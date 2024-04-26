aca36c3033ba2dc6df82080564423596e3b57fa1
Ollinar <106917997+Ollinar@users.noreply.github.com>
2024-04-27 00:21:43 +0800

    schema and git ignore patch

--- taskmanagerdb.sql
+++ taskmanagerdb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2023 at 12:02 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Apr 26, 2024 at 06:20 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,42 +24,104 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `alltask`
+-- Table structure for table `logs`
 --
 
-CREATE TABLE `alltask` (
-  `taskID` int(11) NOT NULL,
-  `taskName` varchar(100) NOT NULL,
-  `taskDesc` varchar(255) NOT NULL,
-  `status` varchar(10) NOT NULL,
-  `importance` tinyint(1) NOT NULL,
-  `dueDate` date NOT NULL
+CREATE TABLE `logs` (
+  `task_id` int(11) NOT NULL,
+  `operation` varchar(100) NOT NULL,
+  `happened_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `label` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `alltask`
+-- Dumping data for table `status`
 --
 
+INSERT INTO `status` (`id`, `label`) VALUES
+(1, 'Pending'),
+(2, 'Completed'),
+(3, 'Deleted');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `taskID` int(11) NOT NULL,
+  `taskName` varchar(100) NOT NULL,
+  `taskDesc` varchar(255) NOT NULL,
+  `status_id` int(11) NOT NULL,
+  `importance` tinyint(1) NOT NULL,
+  `dueDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `alltask`
+-- Indexes for table `logs`
 --
-ALTER TABLE `alltask`
-  ADD PRIMARY KEY (`taskID`);
+ALTER TABLE `logs`
+  ADD KEY `logs_tasks_FK` (`task_id`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`taskID`),
+  ADD KEY `alltask_status_FK` (`status_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `alltask`
+-- AUTO_INCREMENT for table `status`
 --
-ALTER TABLE `alltask`
-  MODIFY `taskID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `taskID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `logs`
+--
+ALTER TABLE `logs`
+  ADD CONSTRAINT `logs_tasks_FK` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`taskID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `alltask_status_FK` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
